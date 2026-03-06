6300bea266cae5170ab6f20d004b3ee2204b7d54
Jun Luo <4catcode@gmail.com>
2026-03-06 14:56:21 +0800

    test

---
+++ src/main/java/com/example/demodependentsgradlekotlin/DemoDependentsGradleKotlinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodependentsgradlekotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDependentsGradleKotlinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDependentsGradleKotlinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demodependentsgradlekotlin/DemoDependentsGradleKotlinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodependentsgradlekotlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDependentsGradleKotlinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
